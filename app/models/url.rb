74820c16b11d99a7888004018861988ca59ae67d
channa5 <channabasava1098@gmail.com>
2025-01-22 21:12:03 +0530

    Created project folder setup, table migartion and Model file changes

---
+++ app/models/url.rb
@@ -0,0 +1,14 @@
+class Url < ApplicationRecord
+  before_create :generate_short_url
+
+  validates :original_url, presence: true, format: URI::regexp(%w[http https])
+
+  private
+
+  def generate_short_url
+    loop do
+      self.short_url = SecureRandom.urlsafe_base64(6)
+      break unless Url.exists?(short_url: short_url)
+    end
+  end
+end
